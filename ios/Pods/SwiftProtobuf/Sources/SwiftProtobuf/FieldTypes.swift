314beea9d114bab3f8c097125ebc90383990896c
Jorge Layana <jlayanab@icloud.com>
2021-11-10 21:25:07 -0500

    Arreglo de PodFile

--- ios/Pods/SwiftProtobuf/Sources/SwiftProtobuf/FieldTypes.swift
+++ ios/Pods/SwiftProtobuf/Sources/SwiftProtobuf/FieldTypes.swift
@@ -4,7 +4,7 @@
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See LICENSE.txt for license information:
-// https://github.com/apple/swift-protobuf/blob/master/LICENSE.txt
+// https://github.com/apple/swift-protobuf/blob/main/LICENSE.txt
 //
 // -----------------------------------------------------------------------------
 ///
@@ -50,6 +50,17 @@ public protocol FieldType {
 /// Marker protocol for types that can be used as map keys
 ///
 public protocol MapKeyType: FieldType {
+    /// A comparision function for where order is needed.  Can't use `Comparable`
+    /// because `Bool` doesn't conform, and since it is `public` there is no way
+    /// to add a conformance internal to SwiftProtobuf.
+    static func _lessThan(lhs: BaseType, rhs: BaseType) -> Bool
+}
+
+// Default impl for anything `Comparable`
+extension MapKeyType where BaseType: Comparable {
+    public static func _lessThan(lhs: BaseType, rhs: BaseType) -> Bool {
+        return lhs < rhs
+    }
 }
 
 ///
@@ -362,6 +373,14 @@ public struct ProtobufBool: FieldType, MapKeyType, MapValueType {
     }
     public static func visitPacked<V: Visitor>(value: [BaseType], fieldNumber: Int, with visitor: inout V) throws {
         try visitor.visitPackedBoolField(value: value, fieldNumber: fieldNumber)
+    }
+
+    /// Custom _lessThan since `Bool` isn't `Comparable`.
+    public static func _lessThan(lhs: BaseType, rhs: BaseType) -> Bool {
+        if !lhs {
+            return rhs
+        }
+        return false
     }
 }
 
